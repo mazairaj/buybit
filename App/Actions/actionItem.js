3e162e9068138fe2efe6cb86335ea9b51d23931c
Junjie Jiang <junjiejiang94@gmail.com>
2018-05-23 05:48:08 -0400

    itemForm

--- App/Actions/actionItem.js
+++ App/Actions/actionItem.js
@@ -187,7 +187,7 @@ function errors(err) {
 }
 
 //get exchange ratio
-export function storeLookUp() {
+export function exchangeRateLookUp() {
     return dispatch => {
         // "Enviroment.server = http:local:8080/"
         fetch(Enviroment.SERVER + 'getExchange', {
@@ -217,8 +217,3 @@ function exchange_rate(exchangeRate) {
         exchangeRate
     };
 }
-
-// router.get('/getExchange'){
-//     .......
-//     res.send(exchangeRate)
-// }
