0209be711be0c68be97250903b5aefb3c90fa824
Saena <tsukasa131@gmail.com>
2018-05-22 20:41:45 -0400

    Correct Call and Return

--- ItemAction.js
+++ ItemAction.js
@@ -3,13 +3,9 @@ const cc = require('cryptocompare');
 
 var ItemAction = function() {
     //get current price in ETH from fromCurrency value, could be user setting
-    ToETH = function (fromCurrency) {
+    ToETH = function (ItemPrice, fromCurrency) {
         cc.price('ETH', fromCurrency)
             .then(prices => {
-            //Sample Item Price, might pass in ItemValue, or have this be part of the Item class so will see ItemPrice
-            var ItemPrice = 100;
-
-            //Might also need to convert item price for USD to whichever currency
 
         switch(fromCurrency) {
 
@@ -17,31 +13,31 @@ var ItemAction = function() {
                 //Grab ETH price in USD, and Divide into ItemPrice
                 var ETHPrice = prices.USD;
                 var convertedPrice = ItemPrice / ETHPrice;
-                console.log(convertedPrice);
+                return convertedPrice;
                 break;
             case 'EUR' :
                 //Grab ETH price in EUR, and Divide into ItemPrice
                 var ETHPrice = prices.EUR;
                 var convertedPrice = ItemPrice / ETHPrice;
-                console.log(convertedPrice);
+                return convertedPrice;
                 break;
             case 'GBP' :
                 //Grab ETH price in EUR, and Divide into ItemPrice
                 var ETHPrice = prices.GBP;
                 var convertedPrice = ItemPrice / ETHPrice;
-                console.log(convertedPrice);
+                return convertedPrice;
                 break;
             case 'JPY' :
                 //Grab ETH price in JPY, and Divide into ItemPrice
                 var ETHPrice = prices.JPY;
                 var convertedPrice = ItemPrice / ETHPrice;
-                console.log(convertedPrice);
+                return convertedPrice;
                 break;
             case 'KRW' :
                 //Grab ETH price in KRW, and Divide into ItemPrice
                 var ETHPrice = prices.KRW;
                 var convertedPrice = ItemPrice / ETHPrice;
-                console.log(convertedPrice);
+                return convertedPrice;
                 break;
         }
     })
@@ -49,11 +45,11 @@ var ItemAction = function() {
     };
 
     return {
-        ToETH: function(fromCurrency) {
-            return ToETH(fromCurrency);
+        ToETH: function(ItemPrice, fromCurrency) {
+            return ToETH(ItemPrice, fromCurrency);
         }
     }
 };
 module.exports = ItemAction;
-ItemAction = new ItemAction;
-ItemAction.ToETH('USD');
+//example call: ItemAction.ToEth(100, 'USD');
+//returns Item Price in ETH
